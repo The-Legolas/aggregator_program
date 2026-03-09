4d35ad6b0fcacea43c0957b1b657398f4b4b5590
The_Legolas <lucalegom@gmail.com>
2026-03-09 23:06:59 +0100

    Created the first sql table up migration for the database

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT UNIQUE NOT NULL
+);
+
+
+-- +goose Down
+DROP TABLE users;
+
+
+--psql "postgres://the_legolas_2:postgres@localhost:5432/gator"
